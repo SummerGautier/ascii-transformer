fd7e8ef3f65fb9f98c3215407bebc4c0c44bdeae
Summer Gautier <summergautier@gmail.com>
2025-02-19 05:37:47 -0600

    outlined ArgumentParser class. Implement tommorrow

--- src/ConfigBuilder.hpp
+++ src/ConfigBuilder.hpp
@@ -3,6 +3,30 @@
 #include <string>
 #include "Config.hpp"
 
+// parses command-line arguments
+class ArgumentParser {
+	public:
+		// create argument parser. reset index and current arg.
+		ArgumentParser (int _argCount, char* _argVector);
+		// increments index and returns next arg.
+		std::string Next ();
+		// returns true if index is less than arg count.
+		bool hasNext ();
+		// returns current index and current arg.
+		std::pair<int, std::string> Current ();
+	private:
+		// total passed arguments from cli.
+		int argCount;
+		// vector of all passed arguments from cli.
+		char* argVector[] {};
+		// current argument value.
+		std::string current {""};
+		// current index.
+		int index {0};
+		// limit attempts to pass massive arg vector to the program.
+		constexpr int MaxAllowedArgs {5};
+};
+
 // used to verify and parse cli arguments in config object.
 namespace ConfigBuilder {
 	// status of each step in config build operation.
@@ -11,8 +35,8 @@ namespace ConfigBuilder {
 		Failed,
 		Completed
 	};
-	// limit attempts to pass massive arg vector to the program.
-	constexpr int MaxAllowedArgs {5};
+
+
 	// used to pass ref to config for building.
 	bool Build (Config& _configRef, int _argCount, char* _argVector[]);
 	// extract argument passed from input stream via main(). Throws runtime_error on max arg count.
@@ -28,3 +52,4 @@ namespace ConfigBuilder {
 	// confirm config data is expected types and ranges.
 	Status ValidateConfig (Config& _configRef);
 };
+
