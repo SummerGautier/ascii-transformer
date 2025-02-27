717051ceb4b78fb8ad6a321ad8cda8ec8033ef78
Summer Gautier <summergautier@gmail.com>
2025-02-26 23:10:36 -0600

    linter

--- src/Main.cpp
+++ src/Main.cpp
@@ -25,50 +25,50 @@ int main (int _argCount, char* _argVector[]) {
 	 * Write Transform to Output File
 	 **/
 
-	// { // contained scope for file manipulation.
+	{ // contained scope for file manipulation.
 
-	// 	//open input and output streams.
-	// 	std::ifstream _ifstream {
-	// 		_config.sourceFile.FileName(),
-	// 		std::ios::in
-	// 	};
+		//open input and output streams.
+		std::ifstream _ifstream {
+			_config.sourceFile.FileName(),
+			std::ios::in
+		};
 
-	// 	if (!_ifstream.is_open()) {
-	// 		throw std::runtime_error(std::format("\n Unable to open file {} \n Double check the file path.", _config.sourceFile.FileName()));
-	// 	}
+		if (!_ifstream.is_open()) {
+			throw std::runtime_error(std::format("\n Unable to open file {} \n Double check the file path.", _config.sourceFile.FileName()));
+		}
 
-	// 	std::ofstream _ofstream {
-	// 		_config.outputFile.FileName(),
-	// 		std::ios::out
-	// 	};
+		std::ofstream _ofstream {
+			_config.outputFile.FileName(),
+			std::ios::out
+		};
 
-	// 	if (!_ofstream.is_open()) {
-	// 		throw std::runtime_error(std::format("\n Unable to open file {} \n Double check the file path.", _config.outputFile.FileName()));
-	// 	}
+		if (!_ofstream.is_open()) {
+			throw std::runtime_error(std::format("\n Unable to open file {} \n Double check the file path.", _config.outputFile.FileName()));
+		}
 
-	// 	AsciiDraw _transform = AsciiDraw();
+		AsciiDraw _transform = AsciiDraw();
 
-	// 	char _current {' '};
+		char _current {' '};
 
-	// 	while (!_ifstream.eof()) {
+		while (!_ifstream.eof()) {
 
-	// 		if (_ifstream.bad()) {
-	// 			throw std::runtime_error("I/O error while reading.");
-	// 		}
-	// 		if (_ifstream.fail()) {
-	// 			throw std::runtime_error("Corrupted data encountered.");
-	// 		}
+			if (_ifstream.bad()) {
+				throw std::runtime_error("I/O error while reading.");
+			}
+			if (_ifstream.fail()) {
+				throw std::runtime_error("Corrupted data encountered.");
+			}
 
-	// 		_current = _ifstream.get();
+			_current = _ifstream.get();
 
-	// 		if (!_ifstream.eof()) {
-	// 			std::cout << _current << std::endl;
-	// 		}
-	// 	}
+			if (!_ifstream.eof()) {
+				std::cout << _current << std::endl;
+			}
+		}
 
-	// 	_ifstream.close();
-	// 	_ofstream.close();
-	// }
+		_ifstream.close();
+		_ofstream.close();
+	}
 
 	// read in input file one charachter at a time.
 	// for each charachter
